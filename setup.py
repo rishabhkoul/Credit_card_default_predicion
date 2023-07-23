1b00e0561010ced8092f8a6cd601e2565164ae28
rishabhkoul <rishabhkoul@ggmail.com>
2023-07-23 13:21:50 +0530

    updated setup.py file

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import find_packages,setup
 
-def get_requirements():
-    pass
-
-
+REQUIREMENT_FILE_NAME = "requirements.txt"
+HYPHEN_E_DOT = '-e .'
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [i.replace("\n","")for i in requirement_list]
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list    
 
 setup(
     name = "credit",
