b091084f179091075d14b2d71cd03660d1bd9ca4
rishabhkoul <rishabhkoul@ggmail.com>
2023-07-23 13:07:15 +0530

    uppdated requirements and added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages,setup
+
+def get_requirements():
+    pass
+
+
+
+setup(
+    name = "credit",
+    version = "0.0.1",
+    author = "rishabhkoul",
+    author_name = "rishabhkoul@gmail.com",
+    packages = find_packages( ),
+    install_requires = get_requirements()
+)
